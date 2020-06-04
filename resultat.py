59b8523e7089b6127dfc4c4af384bfb2d832a58e
Raphaelle Gauchee <rapha.gauchee@gmail.com>
2020-06-05 00:06:33 +0200

    affichage des bons resultats

--- resultat.py
+++ resultat.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 def main() :
     time = [10, 20, 25, 30, 50, 100, 150]
     Brute_Force = [0, 2.1, 6.0, 60*41, 60*41, 60*41, 60*41]
-    LLL = [0.03, 0.2, 0.4, 0.7, 4.4, 86, 551]
+    LLL = [0.03, 0.2, 0.4, 0.7, 4.4, 73, 440]
 
     plt.xlabel("Taille de la liste")
     plt.ylabel("Temps en seconde")
